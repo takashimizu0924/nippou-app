f17e52f99aa7ab6aab6885ff06f62b47e777e2f6
Hiroshimizu0706 <hiroshi222432@gmail.com>
2023-06-16 22:15:46 +0900

    ウィンドウウィジェットパッケージ内のファイル名を変更、共通パッケージにアノテーション用インポート処理を追加

--- src/pkg_common/src/annotations.py
+++ src/pkg_common/src/annotations.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Dict, List, Optional, Tuple, Union
