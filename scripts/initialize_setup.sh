97ecc329151733621c53f3398f4839e9e328986f
Hiroshimizu0706 <hiroshi222432@gmail.com>
2023-06-18 16:17:08 +0900

    アプリのセットアップ用スクリプトを修正

--- scripts/initialize_setup.sh
+++ scripts/initialize_setup.sh
@@ -3,25 +3,35 @@
 ### シェルスクリプトのエントリー
 # スクリプト用変数定義
 START_COMMENT="INITIALIZING START"
-OK_COMMENT="\e[34;1mOK\e[m"
-NG_COMMENT="\e[31;1mNG\e[m"
+END_COMMENT="INITIALIZING FINISHED"
+OK_COMMENT="\e[34;42;1m OK \e[m"
+NG_COMMENT="\e[37;41;1m NG \e[m"
+SUCCESS_COMMENT="\e[32;44;1m SUCCESS \e[m"
+FAILED_COMMENT="\e[33;41;1m FAILED \e[m"
+echo ""
+echo -e "\e[30;47;1m ----------------------------------------------------------- \e[m"
+echo -e "\e[30;47;1m -------------------- ${START_COMMENT} ------------------- \e[m"
+echo -e "\e[30;47;1m ----------------------------------------------------------- \e[m"
 
-echo -e "\e[44;1m -----${START_COMMENT}----- \e[m"
-
+### 処理開始時刻表示
+START_TIME=`date "+%Y/%m/%d %H:%M:%S"`
+echo "Finished time--------------> [${START_TIME}]"
+echo ""
 
 ### ビルド前にコマンド存在チェック
 # スクリプト内で利用するコマンド名配列を定義
 COMMAND_NAME_LIST[0]="python3"
-COMMAND_NAME_LIST[1]="pip"
+COMMAND_NAME_LIST[2]="pip3"
 
 # コマンドチェック実行
 for command in ${COMMAND_NAME_LIST[@]}
     do
         # コマンド存在チェック実行
         if type ${command} > /dev/null 2>&1; then
-            echo -e "[\e[33;1mCOMMAND-EXISTS-CHECKING\e[m]:----->[${OK_COMMENT}]----------'${command}'"
+            echo -e "[\e[33;1mCOMMAND-EXISTS-CHECKING\e[m]:---------->[${OK_COMMENT}]----------'${command}'"
         else
-            echo -e "[\e[33;1mCOMMAND-EXISTS-CHECKING\e[m]:----->[${NG_COMMENT}]----------'${command}'"
+            echo -e "[\e[33;1mCOMMAND-EXISTS-CHECKING\e[m]:---------->[${NG_COMMENT}]----------'${command}'"
+            echo -e "[\e[31;1m'${command} is not found. Please install '${command}' and retry.\e[m"
             exit -1
         fi
     done
@@ -32,35 +42,88 @@ for command in ${COMMAND_NAME_LIST[@]}
 IS_NOT_EXISTS=false
 # スクリプト内で利用するPythonパッケージ確認コマンド配列を定義
 PIP_PKG_NAME_LIST[0]="sdist "
-PIP_PKG_NAME_LIST[1]="pandas "
 
-# コマンドチェック実行
+# パッケージチェック実行
 for package in ${PIP_PKG_NAME_LIST[@]}
     do
-        # コマンド存在チェック実行
+        # パッケージ存在チェック実行
         if pip show ${package} > /dev/null 2>&1; then
-            echo -e "[\e[33;1mPIP-PACKAGE-EXISTS-CHECKING\e[m]:->[${OK_COMMENT}]----------'${package}'" 
+            echo -e "[\e[33;1mPIP-PACKAGE-EXISTS-CHECKING\e[m]:------>[${OK_COMMENT}]----------'${package}'" 
         else
-            echo -e "[\e[33;1mPIP-PACKAGE-EXISTS-CHECKING\e[m]:->[${NG_COMMENT}]----------'${package}'"
-            exit -1
+            echo -e "[\e[33;1mPIP-PACKAGE-EXISTS-CHECKING\e[m]:------>[${NG_COMMENT}]----------'${package}'"
+            IS_NOT_EXISTS=true
         fi
+
+        if ${IS_NOT_EXISTS}; then
+            # パッケージが存在しなければインストール
+            pip install ${package}
+            echo "[\e[37;44m----- Installed '${package}' -----\e[m"
+        fi
+
     done
 
 
 ### 作業ディレクトリへ移動
 cd ../src/
 
-
 ### ビルド対象のパッケージ配列作成
+# 利用する変数定義
+FILE_PATH="./dist/*"
+FILE_NAME=""
 # 追加するパッケージがあれば以下に追加
 PYTHON_PKG_NAME_LIST[0]="pkg_common"
 PYTHON_PKG_NAME_LIST[1]="pkg_db"
 PYTHON_PKG_NAME_LIST[2]="pkg_window_widgets"
 
-
-### Pythonのsetup.pyを呼び出し各パッケージのビルド
+### Pythonのsetup.pyを呼び出し各パッケージのビルド＆インストール
 for PKG in ${PYTHON_PKG_NAME_LIST[@]}
     do
-        # cd ${PKG} && python3 setup.py sdist
-        echo ${PKG}
+        if ! [ -d ${PKG} ]; then
+            # ビルド対象のパッケージディレクトリが存在しない場合はエラー表示して処理スキップ
+            echo -e "\e[31;1mDirectory does not exists.\e[m Directory name: ['\e[30;47;1m ./${PKG} \e[m']"
+            continue
+        fi
+
+        # パッケージディレクトリへ移動
+        cd ${PKG}
+
+        ## パッケージのビルド
+        python3 setup.py sdist
+        if [ $? -eq 0 ]; then
+            echo -e "\e[33;1mCreate ['${PKG}'] is\e[m ${SUCCESS_COMMENT}"
+        else
+            echo -e "\e[31;1mCreate ['${PKG}'] is\e[m ${FAILED_COMMENT}"
+            echo -e "\e[33;1mPlease check if thre are any errors in the './${PKG}/setup.py' file.\e[m"
+            continue
+        fi
+
+        if ! [ -f ${FILE_PATH} ]; then
+            # インストール対象のファイルがない場合はエラー表示して処理スキップ
+            echo -e "\e[31;1mFile does not exists.\e[m File name: ['\e[30;47;1m ${FILE_PATH} \e[m']"
+            continue
+        fi
+
+        ## パッケージのインストール
+        pip3 install ${FILE_PATH}
+        if [ $? -eq 0 ]; then
+            echo -e "\e[33;1mInstall ['${PKG}'] is\e[m ${SUCCESS_COMMENT}"
+
+        else
+            echo -e "\e[31;1mInstall ['${PKG}'] is\e[m ${FAILED_COMMENT}"
+            echo -e "\e[33;1mPlease check if thre are any errors in the './${PKG}/setup.py' file.\e[m"
+            continue            
+        fi
+
+        # 次のパッケージのインストール準備
+        cd ../
     done
+
+### 初期化完了 ###
+echo ""
+echo -e "\e[30;47;1m ----------------------------------------------------------- \e[m"
+echo -e "\e[30;47;1m ------------------ ${END_COMMENT} ------------------ \e[m"
+echo -e "\e[30;47;1m ----------------------------------------------------------- \e[m"
+
+### 処理終了時刻表示
+FINISHED_TIME=`date "+%Y/%m/%d %H:%M:%S"`
+echo "Finished time--------------> [${FINISHED_TIME}]"
