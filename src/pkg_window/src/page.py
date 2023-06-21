0b23b314799ffed12835bf1ef1deac536148ce7a
Hiroshimizu0706 <hiroshi222432@gmail.com>
2023-06-21 18:59:26 +0900

    ファイルマネージャ、ログマネージャをアプリで利用する処理追加

--- src/pkg_window/src/page.py
+++ src/pkg_window/src/page.py
@@ -19,22 +19,42 @@ class Page:
         """
         self._name_tag: str = page_name_tag
         self._frame_list: List[object] = []
-        log_save_path: str = "app.log"
-        file_path = f'../../../../log/{file_name}'
-        self._log: LogManager = LogManager()
+        self._start_
+        # ログマネージャインスタンス生成
+        # self._log: LogManager = LogManager()
 
-    def add_frame(self, frame_list: List[object]):
+    def set_start(self, start_page_frame_dict: Dict[str, object]) -> bool:
+        """スタートページ設定
+            NOTE:以下のパラメータを与えてアプリ開始時のスタートページを設定する
+                {"<スタートページタグ名>": <スタートページフレームオブジェクト>}
+
+        Args:
+            start_page_frame_dict (Dict[str, object]): スタートページ辞書データ
+
+        Returns:
+            bool: _description_
+        """
+
+    def add_frame(self, frame_list: List[object]) -> bool:
         """フレームをページに追加
 
         Args:
             frame_list (List[object]): 追加するフレーム配列
         """
+        ### 応答データ作成
+        _rsp: bool = False
+
         # 引数チェック
         if len(frame_list) <= 0:
-            
-            return
+            print(f'Page class:add_frame method --> Add frame failed. frame_list length is {len(frame_list)}')
+            return _rsp
 
-        self._frame_list
+        # フレーム追加
+        self._frame_list.extend(frame_list)
+        _rsp = True
+
+        # 応答
+        return _rsp
     
     def clear(self) -> None:
         """ページクリア
--- src/tasks/nippo_app_task/scripts/app.py
+++ src/tasks/nippo_app_task/scripts/app.py
@@ -4,21 +4,29 @@
 from pkg_common.file_manager import FileManager
 from pkg_common.log_manager import LogLevel, LogManager
 
-file_name = "app.log"
-file_path = f'../../../../log/{file_name}'
-log = LogManager(file_path)
-log.debug("this is test3")
 
 def app() -> None:
     """日報管理アプリのエントリーポイント
     """
-    # FileManager()
-    # log = LogManager()
-    # log.debug("this is test3")
+    ### ファイルマネージャ生成
+    config_file_name: str = "global_setting.json"
+    config_file_path: str = f'../../../../config/{config_file_name}'
+    file_mng: FileManager = FileManager()
+    ## 設定ファイル読み込み
+    config_data: dict = file_mng.json_read(config_file_path)
+    
+    ### プロジェクト設定情報
+    project_dir_path: str = config_data["PROJECT_DIR_PATH"]
+    
+    ### ログマネージャ生成
+    log_config: dict = config_data["LOG"]
+    log_file_path = f'{project_dir_path}{log_config["SAVE_PATH"]}{log_config["SAVE_FILE_NAME"]}'
+    log = LogManager(log_file_path)
+
     log.debug("日報管理アプリ開始")
+    log.debug(f'Load config data. config file name={config_file_name}, config data={config_data}')
 
 ### アプリケーション実行(GUI起動)
 if __name__ == '__main__':
-    print('Test1')
     app()
     
